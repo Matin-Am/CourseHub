8a5b3b7027c7067cf6f6474ef72005fc1f257924
matin.am <matin.amani101013@gmail.com>
2025-04-09 19:01:12 +0330

    changed views

--- home/models.py
+++ home/models.py
@@ -18,6 +18,7 @@ class Course(models.Model):
     def __str__(self):
         return f"{self.title}"
     
+    
 
 
 
@@ -27,6 +28,10 @@ class Episode(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created",)
+
 
     def __str__(self):
         return f"{self.title} - {self.slug}"
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.views import View
 from .models import Episode , Course
 # Create your views here.
@@ -9,12 +9,12 @@ class HomeView(View):
         episodes = Episode.objects.all()
         courses = Course.objects.all()
         if course_slug:
-            course = Course.objects.get(slug=course_slug)
-            episodes = episodes.filter(course=course)
+            course = get_object_or_404(Course , slug=course_slug)
+            episodes = course.episodes.all()
         return render(request,"home/home.html",{"episodes":episodes,"courses":courses})
     
 
 class VideoDetailView(View):
     def get(self,request , vid_slug):
-        episode = Episode.objects.get(slug=vid_slug)
+        episode = get_object_or_404(Episode , slug=vid_slug)
         return render(request,"home/detail.html",{"episode":episode})
