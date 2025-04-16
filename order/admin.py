f121325ac53adf809c24e48ae04f48bf31e7f1d2
matin.am <matin.amani101013@gmail.com>
2025-04-16 19:13:32 +0330

    order models have been created

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Order , OrderItem
 # Register your models here.
+
+
+class OrderItemAmin(admin.TabularInline):
+    model = OrderItem
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAmin,)
+    raw_id_fields = ("user",)
+    list_display = ("user","total_price","paid")
+    list_filter = ("created",)
+    search_fields = ("id",)
+
+admin.site.register(Order , OrderAdmin)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE , related_name='orders')
+    total_price = models.IntegerField()
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE , related_name='items')
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title}- {self.order}"
