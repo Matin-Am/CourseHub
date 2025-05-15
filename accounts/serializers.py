d796f94baac4e4fbc74a5de10fa87d663dc51dd0
matin.am <matin.amani101013@gmail.com>
2025-05-15 18:56:23 +0330

    validated some fields of our serializer with validators

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework import validators
 
+
+def clean_username(value):
+    if value == 'admin':
+        raise serializers.ValidationError("Your username cant be admin,Please choose another username")
 
 
 class UserRegisterationSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(required=True,write_only=True)
+    password2 = serializers.CharField(required=True,write_only=True,label='confirm password')
     class Meta:
         model = User
         fields = ("username","email","password","password2")
         extra_kwargs = {
-            "password":{"write_only":True}
+            "password":{"write_only":True,"required":True},
+            "username":{"required":True,"validators":[clean_username,]},
+            "email":{"required":True},
+
         }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match !!!")
+        return data
 
 class UserVerifyCodeSerializer(serializers.Serializer):
     code = serializers.IntegerField(required=True)
