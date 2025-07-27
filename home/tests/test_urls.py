013e895fb239ff2cf248e3fe3ce280504316ef84
Matin.Am <matin.amani101013@gmail.com>
2025-07-27 15:31:16 +0330

    tested home app urls

---
+++ home/tests/test_urls.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse , resolve
+from home.views import HomeView , VideoDetailView
+
+
+class TestHomeUrls(TestCase):
+
+    def test_home_url(self):
+        url =  reverse("home:home")
+        self.assertEqual(resolve(url).func.view_class , HomeView )
+    
+    def test_home_url(self):
+        url =  reverse("home:episodes",kwargs={"course_slug":"django"})
+        self.assertEqual(resolve(url).func.view_class , HomeView )
+
+    def test_video_url(self):
+        url = reverse("home:video", kwargs={"epi_slug":"first_episode"})
+        self.assertEqual(resolve(url).func.view_class , VideoDetailView)
