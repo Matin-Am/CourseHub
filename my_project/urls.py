0b1ba8c1bea66e958ca39ffb6a55dd9c60fbf8b3
Matin.Am <matin.amani101013@gmail.com>
2025-07-27 15:16:29 +0330

    styled home , login and register html templates

--- my_project/urls.py
+++ my_project/urls.py
@@ -33,4 +33,6 @@ urlpatterns = [
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
